ea9cc6d45867889c43bcced7d3ec4e7cc3cffb8a
Jon Morton <jon.morton@gmail.com>
2013-08-06 15:03:22 +0000

    Demonstrate good style and taste.

--- src/lyfe/core.clj
+++ src/lyfe/core.clj
@@ -1,8 +1,8 @@
 (ns lyfe.core
   (:require [clojure.set :as s]))
 
-
 (defn surrounding [point]
+  "Obtain all possible points around given point."
   (let [[x y] point
         xs (range (- x 1) (+ x 2))
         ys (range (- y 1) (+ y 2))
@@ -10,24 +10,29 @@
     ; don't include the point itself
     (s/difference ps #{point})))
 
-
 (defn neighbors [point life]
+  "Obtain all living points around a given point."
   (s/intersection (surrounding point) life))
 
-
 (defn happy [point life]
+  "Determine if a point ought to be happy."
   (<= 2 (count (neighbors point life)) 3))
 
-
 (defn keep [life]
+  "Use the rules of happiness to determine surviving points."
   (set (filter #(happy % life) life)))
 
+(defn neighborhood [life]
+  "Map each point to the number living points around it."
+  (frequencies
+   (reduce into
+     (map seq (map surrounding life)))))
 
 (defn seed [life]
-  (let [neighborhood (frequencies (reduce into (map seq (map surrounding life))))
-        three-nearby (map key (filter #(= 3 (val %)) neighborhood))]
+  "Determine set of points ideally suited for life; points with
+   exactly three neighbors."
+  (let [three-nearby (map key (filter #(= 3 (val %)) (neighborhood life)))]
     (set three-nearby)))
-
 
 (defn step [life]
   (into (seed life) (keep life)))
