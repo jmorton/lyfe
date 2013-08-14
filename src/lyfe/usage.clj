eec8ae7ff550bfaf07bba1e6846b5fb7258a3cf9
Jon Morton <jmorton@customink.com>
2013-08-14 18:30:35 -0400

    Improve usage example.
    - Overcoming dyslexia for how many years now?

--- src/lyfe/usage.clj
+++ src/lyfe/usage.clj
@@ -1,13 +1,13 @@
 (ns lyfe.usage
-  (:require [lyfe.core :as core :refer [step]]))
+  (:require [lyfe.core :as core :refer [step]]
+            [lyfe.identicon :refer [seed fetch]]))
 
 (def doomed  [ [0,0] ])
 (def still   [ [0,1] [1,1] [0,0] [1,0] ])
 (def blinker [ [0,0] [1,0] [2,0] ])
 (def toad    [ [0,1] [1,1] [2,1] [0,0] [0,1] [0,2] ])
 
-(def world (atom (set doomed)))
+(def world (atom (set (seed (fetch "jmorton")))))
 
-(swap! step world)
+(swap! world step)
 
-(lyfe.identicon/seed (fetch "jmorton"))
